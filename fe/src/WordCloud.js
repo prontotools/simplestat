70f076ff4939714903fdd966cd27ce46459133b9
Gatuk S Chattanon <gatukgl@gmail.com>
2018-07-06 23:22:08 +0700

    Prepare to construct data and set state to it :baby_chick:

--- fe/src/WordCloud.js
+++ fe/src/WordCloud.js
@@ -1,19 +1,36 @@
 import React from 'react'
+import axios from 'axios'
 import ReactWordCloud from 'react-wordcloud'
 
-const words = [
-  {word: 'hello', value: 3},
-  {word: 'world', value: 2},
-  {word: 'github', value: 2},
-  {word: 'code', value: 1},
-]
-
 class WordCloud extends React.Component {
+  state = {
+    words: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await axios.get('http://localhost:5000/')
+      const data = await res.data
+      console.log('[WordCloud] data', data)
+      // Need to change here to not hardcoded
+      const words = [
+        {word: 'สวัสดี', value: 3},
+        {word: 'โลก', value: 2},
+        {word: 'github', value: 2},
+        {word: 'code', value: 1},
+      ]
+
+      this.setState({ words })
+    } catch (e) {
+      console.error('Something went wrong', e)
+    }
+  }
+
   render() {
     return (
       <div style={{width: 600, height: 400, border: '1px solid white'}}>
         <ReactWordCloud
-          words={words}
+          words={this.state.words}
           wordCountKey='value'
           wordKey='word'
         />
