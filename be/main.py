c1f823af4a27e817cb7ce777eab2956ad7743052
zkan <kan@prontomarketing.com>
2018-07-06 17:32:59 +0700

    Change endpoint that is able to get more data :bear:

--- be/main.py
+++ be/main.py
@@ -27,30 +27,13 @@ def g():
         access_token=FACEBOOK_USER_ACCESS_TOKEN,
         version='2.7'
     )
-    query_string = 'fields=feed'
+
+    query_string = 'fields=feed{comments{comments{message,created_time}' \
+        ',message,created_time},message,created_time,updated_time}'
     endpoint_url = f'{FACEBOOK_GROUP_ID}?{query_string}'
     feed = graph.request(endpoint_url).get('feed')
 
-    results = []
-    for each in feed.get('data'):
-        post = {}
-        message = each.get('message')
-        if message:
-            post['title'] = message
-            comment_id = each.get('id')
-            query_string = 'fields=comments{like_count,created_time,message}' \
-                ',created_time,message'
-            endpoint_url = f'{comment_id}?{query_string}'
-            comments = graph.request(endpoint_url).get('comments')
-            if comments:
-                post['comments'] = []
-                for comment in comments.get('data'):
-                    comment_message = comment.get('message')
-                    post['comments'].append(comment_message)
-
-        results.append(post)
-
-    return jsonify(results)
+    return jsonify(feed)
 
 
 if __name__ == '__main__':
